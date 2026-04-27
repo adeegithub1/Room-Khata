92b53db6b4c7ae111b82b82a09c272212b38377d
Adarsh srivastav <143135414+adeegithub1@users.noreply.github.com>
2026-04-28 00:33:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, on
 const firebaseConfig = {
   apiKey: "AIzaSyAp6oj_KE0nxfInqVG44P42pYljKVHKaHo",
   authDomain: "room-khata-43cd3.firebaseapp.com",
-  projectId: "room-khata-43cd3"
+  projectId: "room-khata-43cd3",
+  storageBucket: "room-khata-43cd3.firebasestorage.app",
+  messagingSenderId: "739355882640",
+  appId: "1:739355882640:web:8bd01c7b05d8129fa29415",
+  measurementId: "G-3ZJJ8NGT56"
 };
-
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
