f8c0d7ed376d5f097e2a11219a9edbad7c8d2587
Adarsh srivastav <143135414+adeegithub1@users.noreply.github.com>
2026-04-30 18:30:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebas
 import { getFirestore, collection, addDoc, getDocs, deleteDoc, doc, updateDoc, query, where, getDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
-// BHAi YAHAN APNI ASLI FIREBASE KEYS DAALNA MAT BHOOLNA
 const firebaseConfig = {
   apiKey: "AIzaSyAp6oj_KE0nxfInqVG44P42pYljKVHKaHo",
   authDomain: "room-khata-43cd3.firebaseapp.com",
-  projectId: "room-khata-43cd3"
+  projectId: "room-khata-43cd3",
+  storageBucket: "room-khata-43cd3.firebasestorage.app",
+  messagingSenderId: "739355882640",
+  appId: "1:739355882640:web:8bd01c7b05d8129fa29415",
+  measurementId: "G-3ZJJ8NGT56"
 };
 
 const app = initializeApp(firebaseConfig);
