a5ca534d308dff8cd86d7e5efba25058220d23f7
Adarsh srivastav <143135414+adeegithub1@users.noreply.github.com>
2026-05-22 19:14:46 +0530

    Fix Toasts component dismiss function syntax

--- src/views/OwnerDashboardView.jsx
+++ src/views/OwnerDashboardView.jsx
@@ -2247,8 +2247,8 @@ export default function OwnerDashboardView() {
       </div>
 
       {/* Toasts */}
-      <Toasts list={toasts} dismiss={useCallback(id=>setToasts(p=>p.filter(t=>t.id!==id)),[])}/>
-
+<Toasts list={toasts} dismiss={(id) => setToasts(p => p.filter(t => t.id !== id))} />
+      
       {/* Sheets */}
       <AnimatePresence>
         {viewRoom&&<RoomDetailSheet key="rd"
